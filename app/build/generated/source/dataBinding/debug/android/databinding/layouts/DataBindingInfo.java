a4aa1400573a05820964625433a8a4a49e2a0d91
Mayank <dotsquares.f123@gmail.com>
2018-04-27 16:54:57 +0530

    Generate Template Fail on register issue - solved

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="916cb6d7-42c2-4a7e-a38f-594430d915e8", modulePackage="com.example.handsets", sdkRoot="C:\\Users\\admin\\AppData\\Local\\Android\\Sdk",layoutInfoDir="D:\\D Drive\\AndroidStudioProjects\\FP SDK\\FP_SDK_on_Git\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="757dfdf2-7ea2-472e-a0e0-706419afa568", modulePackage="com.example.handsets", sdkRoot="C:\\Users\\admin\\AppData\\Local\\Android\\Sdk",layoutInfoDir="D:\\D Drive\\AndroidStudioProjects\\FP SDK\\FP_SDK_on_Git\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
